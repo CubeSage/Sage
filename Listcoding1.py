3c08a5e755b9e927c2fe1988b2d61b6d1c96ffcc
CubeSage <79872951+CubeSage@users.noreply.github.com>
2021-03-12 13:12:12 -0700

    Add files via upload

--- Listcoding1.py
+++ Listcoding1.py
@@ -10,16 +10,21 @@ Program Goals:
 myList = []
 def mainProgram():
     while True:
-        print("Hello there! Let's work with lists")
-        print("Choose from the follow options. Type a number below")
-        choice = input("1. Add to a list , 2. Return the value at an index position! 3. Quit    ")
-        if choice == "1":
-                addToList()
-        elif choice == "2":
-                indexValues()
-        elif choice == "3":
-            break
-        #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition
+        try:
+            print("Hello there! Let's work with lists")
+            print("Choose from the follow options. Type a number below")
+            choice = input("1. Add to a list , 2. Return the value at an index position! 3. Quit    ")
+            if choice == "1":
+                    addToList()
+            elif choice == "2":
+                    indexValues()
+            elif choice == "3":
+                randomSearch()
+            else:
+                break
+        except:
+            print("Oopsy Daisy! You made a mistake. Try Again?  ")
+            #TO ADD: 1. A way to loop the action, 2. A way to quit, 3. Think of repitition
 
 def addToList():
     print("Adding to a list, Great Choice!")
@@ -30,6 +35,8 @@ def indexValues():
     print("Oooooh! I heard you need a particular piece of data")
     inputPos = input("What index position are you curious about?   ")
     myList[int(indexPos)]
+
+
 
 if __name__ == "__main__":
     mainProgram()
